23ade74b2c1e8838973d9a1d44745e253acbaf05
Adarsh Srivastava <imadarshsri@gmail.com>
2020-09-15 21:20:24 +0530

    Minimum in Rotated Sorted Array

--- cpp_algo_path/binary_search/minimum_in_rotated_sorted_array.cpp
+++ cpp_algo_path/binary_search/minimum_in_rotated_sorted_array.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <vector>
 using namespace std;
+
 /*
 AIM of BS
 1) Find return criteria like maching the center element of array with ele to find in BS
@@ -9,46 +10,39 @@ AIM of BS
 4) Repeat
 
 Procedure :
-1) Check the mid element for x
-2) check whether x lies in sorted or unsorted half
-3) Choose the portion apt.
+1) Check the mid as minimum element
+2) Min. always lies in unsorted portion (Observation)
+3) Choose Unsorted Portiomn of array
 4) Repeat
 */
 
-//Binary Search
-int search(vector<int> &arr, int n, int x){
+
+//Function to find the minimum element index
+int min_index(vector<int> &arr, int n){
   if(n == 1) return 0;
   int l = 0, r = n - 1;
   int mid = l + (r - l)/2;
   while(l <= r){
     mid = l + (r - l)/2;
-    if(arr[mid] == x) 
+    if(arr[(mid + n - 1)%n] > arr[mid] && arr[mid] < arr[(mid + 1)%n]) 
       return mid;
-    if((arr[mid] > arr[r])){ //Right Side Unsorted
-      if((arr[mid] > x && arr[l] <= x)) {
-        r = mid - 1;
-      }
-      else 
-        l = mid + 1;
+    if(arr[mid] > arr[r]){//Right Side Unsorted
+      l = mid + 1;
     }
-    else{ //Left Side Unsorted
-      if((arr[mid] < x && arr[r] >= x))
-        l = mid + 1;
-      else 
-        r = mid - 1;
+    else{//Left Side Unsorted
+      r = mid - 1;
     }
   }
   return mid;
 } 
 
 int main(){
-  int n = 1, x = 1;
+  int n=1;
   cin>>n;
   vector<int> v(n);
   for(int i = 0 ; i < n; i++){
     cin>>v[i];
   }
-  cin>>x;
-  cout<<search(v, n, x)<<endl;
+  cout<<min_index(v, n)<<endl;
   return 0;
 }
