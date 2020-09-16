6c5732e95b3c7ec83272324d282f1a1db7ecb502
Adarsh Srivastava <imadarshsri@gmail.com>
2020-09-16 09:47:01 +0530

    Search in BS

--- cpp_algo_path/binary_search/search_in_rotated_sorted_array.cpp
+++ cpp_algo_path/binary_search/search_in_rotated_sorted_array.cpp
@@ -15,7 +15,7 @@ Procedure :
 4) Repeat
 */
 
-//Binary Search
+//Method 1
 int search(vector<int> &arr, int n, int x){
   if(n == 1) return 0;
   int l = 0, r = n - 1;
@@ -41,6 +41,42 @@ int search(vector<int> &arr, int n, int x){
   return mid;
 } 
 
+//Method 2
+//Binary Search
+int binary_search(vector<int> &arr, int l , int r, int x){
+  while(l <= r){
+    int mid = l + (r - l)/2;
+    if(arr[mid] == x){
+      return mid;
+    }
+    else if(arr[mid] > x){
+      r = mid - 1;
+    }
+    else {
+      l = mid + 1;
+    }
+  }
+  return -1;
+}
+//Find min in Rotated Sorted Array
+int find_min(vector<int> &arr, int l, int r){
+  int n = arr.size();
+  if( n == 1) return arr[0];
+  while(l <= r){
+    int mid = l + (r - l)/2;
+    if(arr[(mid + n - 1) % n] > arr[mid] && arr[mid] < arr[(mid + 1) % n]){
+      return mid;
+    }
+    else if(arr[mid] > arr[r]){
+      l = mid + 1;
+    }
+    else{
+      r = mid - 1;
+    }
+  }
+  return -1;
+}
+
 int main(){
   int n = 1, x = 1;
   cin>>n;
@@ -49,6 +85,13 @@ int main(){
     cin>>v[i];
   }
   cin>>x;
-  cout<<search(v, n, x)<<endl;
+  //Method 1
+  // cout<<search(v, n, x)<<endl;
+  //Method 2
+  int idx = find_min(v,0,n-1);
+  int ans1 = binary_search(v, 0, idx - 1, x);
+  int ans2 = binary_search(v, idx, n - 1, x);
+  int ans = (ans1 == -1) ? ans2 : ans1;
+  cout<<ans<<endl;
   return 0;
 }
